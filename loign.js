510afadee87da6babd6e5f522393322d8a2673f5
abdalhafezsayeh <abdalahfezsayeh1997@gmail.com>
2022-08-31 01:12:52 +0200

    Add Fut About Loign In And Up

---
+++ loign.js
@@ -0,0 +1,24 @@
+
+
+
+var inlog = document.getElementById("in")
+var uplog = document.getElementById("up")
+
+// Function Switch To Up 
+function inloglog()
+{
+    console.log("testr")
+    uplog.style.transform = "translateX(600px)";
+    inlog.style.transform = "translateX(600px)";    
+
+
+}
+// Function Switch To In
+function uploglog()
+{
+    console.log("testr")
+    uplog.style.transform = "translateX(0)";
+    inlog.style.transform = "translateX(-600px)";
+
+
+}
