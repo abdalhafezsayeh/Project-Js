480c1d3939cbfb2449dde8ee22b0d658c664761c
abdalhafezsayeh <abdalahfezsayeh1997@gmail.com>
2022-09-01 00:32:36 +0200

    Add Fut Search And Sort And Tages With Api

--- js/loign.js
+++ js/loign.js
@@ -5,7 +5,7 @@ var uplog = document.getElementById("up")
 // Function One Switch To Up 
 function inloglog()
 {
-    console.log("testr")
+    // console.log("testr") // Test 
     uplog.style.transform = "translateX(600px)";
     inlog.style.transform = "translateX(600px)";    
 
@@ -20,33 +20,58 @@ function uploglog()
 
 
 }
-
-
 // Start Chick About Form 
 var fName = document.getElementById("fname") // UserName
 var femail = document.getElementById("ename") // Email 
 var pasOne = document.getElementById("pasOne") // Pas One 
 var pasTwo = document.getElementById("pasTwo") // Pas Two 
-
+// Start Function Sign Up 
 function sginUp()
 {
 
-    // var pOne = pasOne.value;
-    // var pTwo = pasTwo.value;
-
-    if(pasOne.value == pasTwo.value)
+    if(pasOne.value.length > 5 && pasTwo.value.length > 5 && fName.value.length >= 5)
     {
-        console.log("true") // Test 
-        localStorage.setItem("username", fName.value)
-        localStorage.setItem("email", femail.value)
-        uplog.setAttribute("action","./index.html")
-
-    } else {
-
-        console.log("false") // Test 
-        
-        uplog.setAttribute("action","#")
+        if(pasOne.value == pasTwo.value)
+        {        
+            console.log("true") // Test 
+            localStorage.setItem("username", fName.value)
+            localStorage.setItem("email", femail.value)
+            localStorage.setItem("pas", pasOne.value)
+            uplog.setAttribute("action","./index.html")
+    
+        } else {
+    
+            console.log("false") // Test 
+            
+            uplog.setAttribute("action","#")
+            alert("Not Valid")
+        }
     }
+
+
+}
+
+
+// Input Inteface 
+var userNameOne = document.querySelector("#lnameTwo") // User Name 
+var userPasOne = document.querySelector("#lPasTwo") // User Password
+    // local Storage 
+    var UserEmailFromLocalStorage = localStorage.getItem("email") // Email From Local Storage
+    var UserPasFromLocalStorage = localStorage.getItem("pas") // Password From Local Storage
+function sginIn()
+{
+   
+    if(userNameOne.value == UserEmailFromLocalStorage && userPasOne.value == UserPasFromLocalStorage)
+    {
+        inlog.setAttribute("action","./index.html")
+
+    } else 
+    {
+        alert("There is no account with this data")
+      
+    }
+   
+    
 }
 
 
--- js/shop.js
+++ js/shop.js
@@ -1,51 +1,183 @@
 
-function funcOne() {
-
 // JSON 
+var ttest = document.getElementById("ttest") // Select Options
+ttest.addEventListener("click",function(e){
 
-document.addEventListener("click",function(e){
+ 
 
-
-        var lo = e.target.value;  
-
-
+        // console.log(e.target) // Test 
    
 
+    var booksJson = new XMLHttpRequest();  // Step One Initilaiz 
+    booksJson.open("GET","https://www.googleapis.com/books/v1/volumes?q=" + e.target.value );  // Step Two Server Connection 
 
-var booksJson = new XMLHttpRequest();  // Step One Initilaiz 
-booksJson.open("GET","https://www.googleapis.com/books/v1/volumes?q=" + lo );  // Step Two Server Connection 
-
-booksJson.onreadystatechange = function()
-{
-    if(booksJson.readyState == 4 && booksJson.status == 200)
+    booksJson.onreadystatechange = function()
     {
-        var arr = JSON.parse(booksJson.responseText)
-
-
-
-        // console.log(arr.items.length) // Test 
-        for(var i = 0; i<arr.items.length; i++ )
+        if(booksJson.readyState == 4 && booksJson.status == 200)
         {
-            // console.log(arr.items[i].volumeInfo.title) // Test 
+            var arr = JSON.parse(booksJson.responseText)
 
-            
-            // console.log( document.querySelectorAll(".righttext h2")[i]) // Test 
 
-            document.querySelectorAll(".righttext h2")[i].innerHTML = arr.items[i].volumeInfo.title
-            document.querySelectorAll(".righttext .authors")[i].innerHTML = arr.items[i].volumeInfo.authors
+
+            // console.log(arr.items.length) // Test 
+            for(var i = 0; i<arr.items.length; i++ )
+            {
+                // console.log(arr.items[i].volumeInfo.title) // Test 
+
+                
+                // console.log( document.querySelectorAll(".righttext h2")[i]) // Test 
+
+                document.querySelectorAll(".righttext h2")[i].innerHTML = arr.items[i].volumeInfo.title
+                document.querySelectorAll(".righttext .authors")[i].innerHTML = arr.items[i].volumeInfo.authors
+                document.querySelectorAll(".leftimg img")[i].src = arr.items[i].volumeInfo.imageLinks.smallThumbnail
+                document.querySelectorAll("h4 span")[i].innerHTML = arr.items[i].volumeInfo.publishedDate
+                document.querySelectorAll(".righttext p:last-of-type")[i].innerHTML = arr.items[i].volumeInfo.description
+                // Add Attribute Title In Pragraph
+                document.querySelectorAll(".righttext p:last-of-type")[i].setAttribute("title", arr.items[i].volumeInfo.description ) 
+                // console.log(document.querySelectorAll("h4 span")[0])
+                // console.log(arr.items[i].volumeInfo.description)
+                // console.log(document.querySelectorAll(".leftimg img")[i].src)
+                // console.log( arr.items[i].volumeInfo.imageLinks.smallThumbnail)
+
+            }
+
+
 
         }
 
+    }
+    booksJson.send()
 
+
+
+
+
+}) // End The Event Listener
+
+
+
+
+
+// Start PRODUCT TAGES 
+
+// JSON 
+var tages = document.getElementById("tagg") // Select Options
+
+tages.addEventListener("click",function(e){
+
+    console.log(e.target.title)
+
+        // console.log(e.target) // Test 
+   
+
+    var booksJson = new XMLHttpRequest();  // Step One Initilaiz 
+    booksJson.open("GET","https://www.googleapis.com/books/v1/volumes?q=" + e.target.title );  // Step Two Server Connection 
+
+    booksJson.onreadystatechange = function()
+    {
+        if(booksJson.readyState == 4 && booksJson.status == 200)
+        {
+            var arr = JSON.parse(booksJson.responseText)
+
+
+
+            // console.log(arr.items.length) // Test 
+            for(var i = 0; i<arr.items.length; i++ )
+            {
+                // console.log(arr.items[i].volumeInfo.title) // Test 
+
+                
+                // console.log( document.querySelectorAll(".righttext h2")[i]) // Test 
+
+                document.querySelectorAll(".righttext h2")[i].innerHTML = arr.items[i].volumeInfo.title
+                document.querySelectorAll(".righttext .authors")[i].innerHTML = arr.items[i].volumeInfo.authors
+                document.querySelectorAll(".leftimg img")[i].src = arr.items[i].volumeInfo.imageLinks.smallThumbnail
+                document.querySelectorAll("h4 span")[i].innerHTML = arr.items[i].volumeInfo.publishedDate
+                document.querySelectorAll(".righttext p:last-of-type")[i].innerHTML = arr.items[i].volumeInfo.description
+                // Add Attribute Title In Pragraph
+                document.querySelectorAll(".righttext p:last-of-type")[i].setAttribute("title", arr.items[i].volumeInfo.description ) 
+                // console.log(document.querySelectorAll("h4 span")[0])
+                // console.log(arr.items[i].volumeInfo.description)
+                // console.log(document.querySelectorAll(".leftimg img")[i].src)
+                // console.log( arr.items[i].volumeInfo.imageLinks.smallThumbnail)
+
+            }
+
+
+
+        }
 
     }
+    booksJson.send()
 
-}
-booksJson.send()
 
-})
 
-}
+
+
+}) // End The Event Listener
+
+
+
+
+
+// Start Search
+// JSON 
+var searchInput = document.getElementById("searchButton") // Select Options
+var bookSearch = document.getElementById("searccInput")
+searchInput.addEventListener("click",function(){
+
+   
+    console.log(bookSearch.value)
+
+        // console.log(e.target) // Test 
+   
+
+    var booksJson = new XMLHttpRequest();  // Step One Initilaiz 
+    booksJson.open("GET","https://www.googleapis.com/books/v1/volumes?q=" + bookSearch.value );  // Step Two Server Connection 
+
+    booksJson.onreadystatechange = function()
+    {
+        if(booksJson.readyState == 4 && booksJson.status == 200)
+        {
+            var arr = JSON.parse(booksJson.responseText)
+
+
+
+            // console.log(arr.items.length) // Test 
+            for(var i = 0; i<arr.items.length; i++ )
+            {
+                // console.log(arr.items[i].volumeInfo.title) // Test 
+
+                
+                // console.log( document.querySelectorAll(".righttext h2")[i]) // Test 
+
+                document.querySelectorAll(".righttext h2")[i].innerHTML = arr.items[i].volumeInfo.title
+                document.querySelectorAll(".righttext .authors")[i].innerHTML = arr.items[i].volumeInfo.authors
+                document.querySelectorAll(".leftimg img")[i].src = arr.items[i].volumeInfo.imageLinks.smallThumbnail
+                document.querySelectorAll("h4 span")[i].innerHTML = arr.items[i].volumeInfo.publishedDate
+                document.querySelectorAll(".righttext p:last-of-type")[i].innerHTML = arr.items[i].volumeInfo.description
+                // Add Attribute Title In Pragraph
+                document.querySelectorAll(".righttext p:last-of-type")[i].setAttribute("title", arr.items[i].volumeInfo.description ) 
+                // console.log(document.querySelectorAll("h4 span")[0])
+                // console.log(arr.items[i].volumeInfo.description)
+                // console.log(document.querySelectorAll(".leftimg img")[i].src)
+                // console.log( arr.items[i].volumeInfo.imageLinks.smallThumbnail)
+
+            }
+
+
+
+        }
+
+    }
+    booksJson.send()
+
+
+
+
+
+}) // End The Event Listener
+
 
 
 
